7bbf0d86d9d09075a871ec28898271b23e087cf6
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-11-24 15:20:44 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,8 +10,6 @@ class PigLatinizer
   # end
 
   def piglatinize(text)
-    # text[/([aeiou].*)/] + text[/([bcdfghjklmnpqrstvwxyz]*)/] + "ay"
-    #  text.scan(/[aeoui]/).count
     words = text.split(" ")
     words.each do |word|
       if word.length > 1 && word[/([AEIOUaeiou]*)/] == ""
