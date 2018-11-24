065fce61edbfa41a369e4dc996e834f6a77f4ca6
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-11-24 15:19:08 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,8 +12,8 @@ class PigLatinizer
   def piglatinize(text)
     # text[/([aeiou].*)/] + text[/([bcdfghjklmnpqrstvwxyz]*)/] + "ay"
     #  text.scan(/[aeoui]/).count
-    @words = text.split(" ")
-    @words.each do |word|
+    words = text.split(" ")
+    words.each do |word|
       if word.length > 1 && word[/([AEIOUaeiou]*)/] == ""
         word[/([AEIOUaeiou].*)/] + word[/([BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz]*)/] + "ay"
       else
